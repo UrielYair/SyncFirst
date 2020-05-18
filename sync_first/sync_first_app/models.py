cf0364ac211997ff9cb3c698249fa24643cc0a08
eleanor <eleanor.rycus@gmail.com>
2020-05-18 13:27:18 +0300

    Added person class

--- sync_first/sync_first_app/models.py
+++ sync_first/sync_first_app/models.py
@@ -4,9 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class Person(models.Model):
+    identification = models.CharField(max_length=9)
+
+
 class Incident(models.Model):
-    main_person = models.CharField(max_length=9)
-    spouse = models.CharField(max_length=9)
+    main_person = models.ForeignKey(Person)
+    spouse = models.ForeignKey(Person)
     date = models.DateField()
     organization = models.CharField(max_length=30)
     description = models.CharField(max_length=120)
