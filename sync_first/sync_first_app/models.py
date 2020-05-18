33b7d56bdcedf8ae860eac2d6a103d75b75efb44
eleanor <eleanor.rycus@gmail.com>
2020-05-18 13:36:44 +0300

    updated models

--- sync_first/sync_first_app/models.py
+++ sync_first/sync_first_app/models.py
@@ -5,13 +5,16 @@ from django.db import models
 
 
 class Person(models.Model):
+    # Should by official id (tz)
     identification = models.CharField(max_length=9)
 
 
 class Incident(models.Model):
     main_person = models.ForeignKey(Person)
     spouse = models.ForeignKey(Person)
-    date = models.DateField()
+    date = models.DateField(auto_add_now=True)
+    # Consider making this a select within given options (valid organizations)
     organization = models.CharField(max_length=30)
-    description = models.CharField(max_length=120)
+    description = models.TextField(max_length=120)
+    # Should by official id (tz)
     reporter = models.CharField(max_length=9, null=True, blank=True)
