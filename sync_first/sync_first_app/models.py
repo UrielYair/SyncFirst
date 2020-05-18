4406eceee8464820104395e5a7298a5155970727
eleanor <eleanor.rycus@gmail.com>
2020-05-18 12:52:05 +0300

    basic models

--- sync_first/sync_first_app/models.py
+++ sync_first/sync_first_app/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Incident(models.Model):
+    main_person = models.CharField(max_length=9)
+    spouse = models.CharField(max_length=9)
+    date = models.DateField()
+    organization = models.CharField(max_length=30)
+    description = models.CharField(max_length=120)
+    reporter = models.CharField(max_length=9, null=True, blank=True)
