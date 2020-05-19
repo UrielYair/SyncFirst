25375513f0b9d0ea20421256ff2813da20eedf8c
Uriel Yair <uriel91@gmail.com>
2020-05-19 04:34:40 +0300

    register Person and Incident.

--- sync_first/sync_first_app/admin.py
+++ sync_first/sync_first_app/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Incident, Person
 
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Incident)
